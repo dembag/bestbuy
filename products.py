50f423d5c2e9a32bafa32291e548e4ffcd06db9b
dembag <jn.denby@gmail.com>
2026-07-21 11:17:18 +0200

    Create product class

---
+++ products.py
@@ -0,0 +1,59 @@
+class Product:
+    def __init__(self, name, price, quantity):
+        self.name = name
+        self.price = price
+        self.quantity = quantity
+        self.active = True
+
+    def get_quantity(self):
+        return self.quantity
+
+    def set_quantity(self, quantity):
+        self.quantity = quantity
+        if quantity <= 0:
+            self.deactivate()
+
+    def is_active(self):
+        return self.active
+
+    def activate(self):
+        self.active = True
+
+    def deactivate(self):
+        self.active = False
+
+    def show(self):
+        print(f"{self.name}, Price: {self.price}, Quantity: {self.quantity}")
+
+    def buy(self, quantity):
+        try:
+            if self.quantity < quantity:
+                raise ValueError("Not enough product to fulfill order!")
+        except ValueError as ve:
+            return ve
+        else:
+            total_price = self.price * quantity
+            remaining_quantity = self.quantity - quantity
+
+            self.set_quantity(remaining_quantity)
+
+            return float(total_price)
+
+
+def main():
+    bose = Product("Bose QuietComfort Earbuds", price=250, quantity=500)
+    mac = Product("MacBook Air M2", price=1450, quantity=100)
+
+    print(bose.buy(50))
+    print(mac.buy(100))
+    print(mac.is_active())
+
+    bose.show()
+    mac.show()
+
+    bose.set_quantity(1000)
+    bose.show()
+
+
+if __name__ == "__main__":
+    main()
