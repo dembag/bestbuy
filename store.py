73d644d0335dda447cb283fa4295fc366661f87c
dembag <jn.denby@gmail.com>
2026-07-22 14:06:13 +0200

    Removed test data from product_list

--- store.py
+++ store.py
@@ -64,8 +64,7 @@ class Store:
 def main():
     product_list = [Product("MacBook Air M2", price=1450, quantity=100),
                     Product("Bose QuietComfort Earbuds", price=250, quantity=500),
-                    Product("Google Pixel 7", price=500, quantity=250),
-                    Product("", price=300, quantity=500),
+                    Product("Google Pixel 7", price=500, quantity=250)
                     ]
 
     best_buy = Store(product_list)
