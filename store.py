0a1439872fc126158afdc9e07e36322568da11d1
dembag <jn.denby@gmail.com>
2026-07-22 11:01:04 +0200

    Added data validation for store inventory initialisation

--- store.py
+++ store.py
@@ -3,6 +3,29 @@ from products import Product
 
 class Store:
     def __init__(self, inventory):
+        for item in inventory:
+            while item.name == "":
+                print("Product name not entered.")
+                print(f"Price: {item.price} Quantity: {item.quantity}")
+                item.name = input("Please enter a product name: ").strip()
+
+            while item.price == 0 or type(item.price) == str:
+                print("Price not entered.")
+                print(f"Name: {item.name} Quantity: {item.quantity}")
+                try:
+                    item.price = float(input("Please enter a price: "))
+                except ValueError:
+                    print("Enter a valid number.")
+
+            while item.quantity == 0 or type(item.quantity) == str:
+                print("Quantity not entered.")
+                print(f"Name: {item.name} Price: {item.price}")
+                try:
+                    item.quantity = int(input("Please enter a quantity: "))
+                    break
+                except ValueError:
+                    print("Enter a valid number: ")
+
         self.inventory = inventory
 
     def add_product(self, product):
@@ -42,13 +65,16 @@ def main():
     product_list = [Product("MacBook Air M2", price=1450, quantity=100),
                     Product("Bose QuietComfort Earbuds", price=250, quantity=500),
                     Product("Google Pixel 7", price=500, quantity=250),
+                    Product("", price=300, quantity=500),
                     ]
 
     best_buy = Store(product_list)
     products = best_buy.get_all_products()
-
+    print(best_buy.inventory)
     print(best_buy.get_total_quantity())
     print(best_buy.order([(products[0], 1), (products[1], 2)]))
+    for product in best_buy.get_all_products():
+        print(product.name, product.price, product.quantity)
 
 
 if __name__ == '__main__':
